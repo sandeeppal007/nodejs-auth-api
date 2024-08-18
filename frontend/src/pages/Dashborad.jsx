d7fbf1e8600258c1fded7b92b22523fe7fb0675e
Sandeep <sandeeppal7902@gmail.com>
2024-08-18 22:00:28 +0530

    dashboard setup

--- frontend/src/pages/Dashborad.jsx
+++ frontend/src/pages/Dashborad.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
 import {motion} from "framer-motion"
+import { useAuthStore } from "../store/authStore";
+import { formatDate } from "../utils/date";
 
 const Dashborad = () => {
+
+  const { user, logout } = useAuthStore();
+
+
+  const handleLogout = () => {
+		logout();
+	};
+
   return (
 <motion.div
 
@@ -24,11 +34,19 @@ className='max-w-md w-full mx-auto mt-10 p-8 bg-gray-900 bg-opacity-80 backdrop-
 
     className='p-4 bg-gray-800 bg-opacity-50 rounded-lg border border-gray-700
     '
-    
-    
-    
-    >
-
+    initial={{ opacity: 0, y: 20 }}
+					animate={{ opacity: 1, y: 0 }}
+					transition={{ delay: 0.4 }}
+				>
+					<h3 className='text-xl font-semibold text-green-400 mb-3'>Account Activity</h3>
+					<p className='text-gray-300'>
+						<span className='font-bold'>Joined: </span>
+						{new Date(user.createdAt).toLocaleDateString("en-US", {
+							year: "numeric",
+							month: "long",
+							day: "numeric",
+						})}
+					</p>
     </motion.div>
   </div>
 
